a548bce7790d4e66eb41abe49e844b5030390077
Harry Garrood <harry@garrood.me>
2014-12-03 05:08:40 +0000

    make it work on Node as well as in browser

--- src/Test/Mocha.purs
+++ src/Test/Mocha.purs
@@ -10,17 +10,20 @@ module Test.Mocha
 import Control.Monad.Eff
 import Data.Foreign.EasyFFI
 
+foreign import globalEnv
+  "var globalEnv = typeof window === 'undefined' ? global : window"
+  :: forall a. a
 
 
 foreign import data Describe  :: !
 type DoDescribe               = forall e a. String -> Eff e a -> Eff (describe :: Describe | e) Unit
 
 describe                      :: DoDescribe
-describe                      = unsafeForeignProcedure ["description", "fn", ""] "window.describe(description, fn);"
+describe                      = unsafeForeignProcedure ["description", "fn", ""] "globalEnv.describe(description, fn);"
 describeOnly                  :: DoDescribe
-describeOnly                  = unsafeForeignProcedure ["description", "fn", ""] "window.describe.only(description, fn);"
+describeOnly                  = unsafeForeignProcedure ["description", "fn", ""] "globalEnv.describe.only(description, fn);"
 describeSkip                  :: DoDescribe
-describeSkip                  = unsafeForeignProcedure ["description", "fn", ""] "window.describe.skip(description, fn);"
+describeSkip                  = unsafeForeignProcedure ["description", "fn", ""] "globalEnv.describe.skip(description, fn);"
 
 
 
@@ -28,11 +31,11 @@ foreign import data It        :: !
 type DoIt                     = forall e a. String -> Eff e a -> Eff (it :: It | e) Unit
 
 it                            :: DoIt
-it                            = unsafeForeignProcedure ["description", "fn", ""] "window.it(description, fn);"
+it                            = unsafeForeignProcedure ["description", "fn", ""] "globalEnv.it(description, fn);"
 itOnly                        :: DoIt
-itOnly                        = unsafeForeignProcedure ["description", "fn", ""] "window.it.only(description, fn);"
+itOnly                        = unsafeForeignProcedure ["description", "fn", ""] "globalEnv.it.only(description, fn);"
 itSkip                        :: DoIt
-itSkip                        = unsafeForeignProcedure ["description", "fn", ""] "window.it.skip(description, fn);"
+itSkip                        = unsafeForeignProcedure ["description", "fn", ""] "globalEnv.it.skip(description, fn);"
 
 foreign import data Done :: !
 data DoneToken = DoneToken
@@ -41,7 +44,7 @@ foreign import itAsync
   "function itAsync(d) {                          \
   \    return function (fn) {                     \
   \       return function(){                      \
-  \         return window.it(d, function(done){   \
+  \         return globalEnv.it(d, function(done){   \
   \           return fn(done)();                  \
   \         });                                   \
   \       };                                      \
@@ -67,14 +70,14 @@ foreign import itIsNot
 
 foreign import data Before     :: !
 before                         :: forall e a. Eff e a -> Eff (before :: Before | e) Unit
-before                         = unsafeForeignProcedure ["fn", ""] "window.before(fn);"
+before                         = unsafeForeignProcedure ["fn", ""] "globalEnv.before(fn);"
 
 beforeEach                     :: forall e a. Eff e a -> Eff (before :: Before | e) Unit
-beforeEach                     = unsafeForeignProcedure ["fn", ""] "window.beforeEach(fn);"
+beforeEach                     = unsafeForeignProcedure ["fn", ""] "globalEnv.beforeEach(fn);"
 
 foreign import data After      :: !
 after                          :: forall e a. Eff e a -> Eff (after :: After | e) Unit
-after                          = unsafeForeignProcedure ["fn", ""] "window.after(fn);"
+after                          = unsafeForeignProcedure ["fn", ""] "globalEnv.after(fn);"
 
 afterEach                      :: forall e a. Eff e a -> Eff (after :: After | e) Unit
-afterEach                      = unsafeForeignProcedure ["fn", ""] "window.afterEach(fn);"
+afterEach                      = unsafeForeignProcedure ["fn", ""] "globalEnv.afterEach(fn);"
